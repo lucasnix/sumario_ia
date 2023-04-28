1a988383fb5e41aa64087c83d441ff6e9f1d07a4
lucasnix <lucasniix@gmail.com>
2023-04-28 17:56:32 -0300

    Commit: GPT4 - Sumário de artigos em IA

---
+++ src/components/Demo.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
+import { Input } from "postcss";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const handleSubmit = async (e) => {
+    alert('Enviado');
+  }
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/*search*/}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Entre uma URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+
+          <button 
+            type="submit" 
+            className="submit_btn
+            peer-focus:border-gray-700
+            peer-focus:text-gray-700">↵</button>
+        </form>
+
+        {/* Browse URL History*/}
+      </div>
+
+        {/* Display results */}
+    </section>
+  );
+};
+
+export default Demo;
